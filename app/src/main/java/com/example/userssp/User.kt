54bb6b784bc85b27c5ffc83af39962f9d73a325d
avilind <xavilind7@gmail.com>
2021-11-25 12:26:16 -0600

    ReclyclerView y DataClass

---
+++ app/src/main/java/com/example/userssp/User.kt
@@ -0,0 +1,15 @@
+package com.cursosant.android.userssp
+
+/****
+ * Project: Fundamentos Kotlin
+ * From: com.cursosant.android.fundamentoskotlin.classes
+ * Created by Alain Nicolás Tello on 10/11/20 at 13:17
+ * Course: Android Practical with Kotlin from zero.
+ * All rights reserved 2021.
+ *
+ * All my Courses(Only on Udemy):
+ * https://www.udemy.com/user/alain-nicolas-tello/
+ ***/
+data class User (val id: Long, var name: String, var lastName: String, var url: String) {
+    
+}
