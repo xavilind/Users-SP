58d92d5f2296a049b3ee6e2b3d9c9a9c7527d87c
avilind <xavilind7@gmail.com>
2021-12-06 12:22:00 -0600

    GenerarDatos

--- app/src/main/java/com/example/userssp/MainActivity.kt
+++ app/src/main/java/com/example/userssp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.cursosant.android.userssp.User
 import com.example.userssp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,12 +21,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        userAdapter = UserAdapter(mutableListOf())
+        userAdapter = UserAdapter(getUsers())
         linearLayoutManager = LinearLayoutManager(this)
 
         binding.recyclerView.apply {
             layoutManager = linearLayoutManager
             adapter = userAdapter
         }
+    }
+
+    private fun getUsers() : MutableList<User>{
+        val users = mutableListOf<User>()
+
+        val alain = User(1, "Alain", "Nicoñas", "")
+        val samanta = User(2, "Samanta", "Meza", "")
+
+        users.add(alain)
+        users.add(samanta)
+
+        return users
     }
 }
