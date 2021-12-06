cc332f40edfa4b3c24581f97d09b4c86fe9975b7
avilind <xavilind7@gmail.com>
2021-12-06 12:34:27 -0600

    ComplementarInformacion

--- app/src/main/java/com/example/userssp/MainActivity.kt
+++ app/src/main/java/com/example/userssp/MainActivity.kt
@@ -33,11 +33,26 @@ class MainActivity : AppCompatActivity() {
     private fun getUsers() : MutableList<User>{
         val users = mutableListOf<User>()
 
-        val alain = User(1, "Alain", "Nicoñas", "")
-        val samanta = User(2, "Samanta", "Meza", "")
+        val alain = User(1, "Alain", "Nicoñas", "https://frogames.es/wp-content/uploads/2020/09/alain-1.jpg")
+        val samanta = User(2, "Samanta", "Meza", "https://www.bing.com/images/search?view=detailV2&ccid=ePuXPpjA&id=FBCAED9538DE5DEF6A26DD67AE8A47E113B5E34F&thid=OIP.ePuXPpjAylrqDh7rBQ8urwHaE8&mediaurl=https%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2fthumb%2fb%2fb2%2fSamanta_villar.jpg%2f800px-Samanta_villar.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.78fb973e98c0ca5aea0e1eeb050f2eaf%3frik%3dT%252bO1E%252bFHiq5n3Q%26pid%3dImgRaw%26r%3d0&exph=534&expw=800&q=samanta&simid=608013145588326924&FORM=IRPRST&ck=C98B2175335EB431DC337055B2E12035&selectedIndex=0&qft=+filterui%3alicense-L2_L3")
+        val javier = User(3, "javier", "Gómez", "https://www.google.com.mx/url?sa=i&url=https%3A%2F%2Fdenaflows.com%2Fha-muerto-javier-krahe-el-mundo-es-un-poco-peor%2F&psig=AOvVaw2AsnBwfhaCJuIkui0mYOKl&ust=1636939455715000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKCs-enYlvQCFQAAAAAdAAAAABAE")
+        val emma = User(4, "Emma", "Cruz", "https://www.google.com.mx/url?sa=i&url=https%3A%2F%2Fwww.wikiwand.com%2Fen%2FEmma_Wortelboer&psig=AOvVaw0I_HfEX9_mjuXRGbJoyeYT&ust=1636939549229000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMjNqJbZlvQCFQAAAAAdAAAAABAD")
 
         users.add(alain)
         users.add(samanta)
+        users.add(javier)
+        users.add(javier)
+        users.add(emma)
+        users.add(alain)
+        users.add(samanta)
+        users.add(javier)
+        users.add(javier)
+        users.add(emma)
+        users.add(alain)
+        users.add(samanta)
+        users.add(javier)
+        users.add(javier)
+        users.add(emma)
 
         return users
     }
